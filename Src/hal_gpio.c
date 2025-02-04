fa5485d5609d34d79fa8643a60eb3faa1f8d5b68
Dallin Hoyt <dhhoyt2002@gmail.com>
2025-02-03 22:06:00 -0700

    partial work

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -1,11 +1,23 @@
 #include <stdint.h>
 #include <stm32f0xx_hal.h>
 #include <stm32f0xx_hal_gpio.h>
-/*
+
+
 void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
 {
-}
-*/
+    // Check validity of parameters (Taken from the library version)
+    assert_param(IS_GPIO_ALL_INSTANCE(GPIOx));
+    assert_param(IS_GPIO_PIN(GPIO_Init->Pin));
+    assert_param(IS_GPIO_MODE(GPIO_Init->Mode));
+    uint32_t pins = GPIO_Init->Pin;
+    for (int i = 0; i < 16; i++) {
+        if ((pins & (1 << i)) == 0) {
+            continue;
+        }
+        
+    }
+}   
+
 
 /*
 void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
